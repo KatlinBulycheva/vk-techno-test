e82b06c78cef5e3e1d9cf449faec6ad62ca88656
Ekaterina Bulycheva <ekaterinabul21@mail.ru>
2023-05-08 18:47:01 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'linebreak-style': 0,
+    'no-use-before-define': 0,
+    'prefer-destructuring': 0,
+    'no-plusplus': 0,
+  },
+};
